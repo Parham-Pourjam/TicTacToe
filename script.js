bf35150264d001bbe65fb43f6ddb752fca46a321
Parham-Pourjam <parhampourjam@cmail.carleton.ca>
2020-09-14 21:45:49 -0400

    Finished win check and board clear after win/tie. Added form element.

--- script.js
+++ script.js
@@ -47,11 +47,97 @@ const gameBoard = (() => {
         }
     };
 
+    const checkForTie = () => {
+        let count = 0;
+        for (let i = 0; i < 9; i++) {
+            if (checkIfCellOccupied(i)) {
+                count++;
+            }
+        }
+        console.log(count);
+        return count;
+    };
+
     const gameStart = () => {
         let playerChoices = [0, 1];
         let playerSelection = playerChoices[Math.floor(Math.random() * playerChoices.length)];
         console.log(playerSelection);
         changePlayer();
+    };
+
+    const checkForWin = (symbol) => {
+       // checking rows for win
+       let rowCount = 0;
+       for (let i = 0; i < _gameBoardArray.length; i++) {
+           for (let j = 0; j < _gameBoardArray.length; j++) {
+               if (symbol == _gameBoardArray[i][j]) {
+                   rowCount += 1;
+               }
+           }
+           if (rowCount == 3) {
+               return true;
+           } else {
+               rowCount = 0;
+           }  
+       }
+
+       // checking columns for win
+       let columnCount = 0;
+       for (let i = 0; i < _gameBoardArray.length; i++) {
+           for (let j = 0; j < _gameBoardArray.length; j++) {
+               if (symbol == _gameBoardArray[j][i]) {
+                   columnCount += 1;
+               }
+           }
+           if (columnCount == 3) {
+               return true;
+           } else {
+               columnCount = 0;
+           }
+       }
+
+       // checking diagonals (bottom left to top right)
+       let diagonalCount = 0;
+       let bottomLeftCellInitialRow = _gameBoardArray.length - 1;
+       let bottomLeftCellInitialColumn = 0;
+       for (let i = 0; i < _gameBoardArray.length; i++) {
+           if (symbol == _gameBoardArray[bottomLeftCellInitialRow][bottomLeftCellInitialColumn]) {
+               diagonalCount += 1;
+               bottomLeftCellInitialColumn++;
+               bottomLeftCellInitialRow--; 
+           }
+       }
+       if (diagonalCount == 3) {
+           return true;
+       } else {
+           diagonalCount = 0;
+           bottomLeftCellInitialColumn =  _gameBoardArray.length - 1;
+            bottomLeftCellInitialRow = 0; 
+       }
+
+       // checking diagonals (top left to bottom right)
+       let diagonalCountx = 0
+       for (let i = 0; i < _gameBoardArray.length; i++) {
+           for (let j = 0; j < _gameBoardArray.length; j++) {
+               if (symbol == _gameBoardArray[i][j] && i == j) {
+                   diagonalCountx += 1;
+               }
+           }
+       }
+       if (diagonalCountx == 3) {
+         
+         return true;
+       } 
+    
+    };
+
+    const clearGameBoardArray = () => {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                _gameBoardArray[i][j] = 0;
+            }
+        }
+        displayController.refreshBoard();
     };
 
     const getGameBoardArray = () => (_gameBoardArray);
@@ -60,7 +146,7 @@ const gameBoard = (() => {
 
     setPlayers("Parham");
 
-    return {setPlayers, changePlayer, getCurrentPlayer, getGameBoardArray, checkIfCellOccupied, gameStart}
+    return {clearGameBoardArray, checkForWin, checkForTie, setPlayers, changePlayer, getCurrentPlayer, getGameBoardArray, checkIfCellOccupied, gameStart}
 })();
 
 // Module pattern for display control
@@ -83,15 +169,34 @@ const displayController = (() => {
         gameBoard.gameStart();
     };
 
+    // clear board after win or tie
+    const refreshBoard = () => {
+        const children = gameContainer.children;
+        const childrenArray = Array.from(children);
+        childrenArray.forEach(item => {
+            item.textContent = "";
+        });
+    };
+
     function addSymbol() {
         //this.textContent = gameBoard.getCurrentPlayer().getSymbol();
         //gameBoard.changePlayer();
+        let symbol = gameBoard.getCurrentPlayer().getSymbol();
+        let player = gameBoard.getCurrentPlayer().getName();
         
         if (updateGameBoardArray(this)) {
             console.log(gameBoard.getCurrentPlayer().getName());
             this.textContent = gameBoard.getCurrentPlayer().getSymbol();
             gameBoard.changePlayer();
             //console.log(gameBoard.getCurrentPlayer().getName());
+        }
+        if (gameBoard.checkForWin(symbol)) {
+            alert(player + " won");
+            gameBoard.clearGameBoardArray();
+        }
+        else if (gameBoard.checkForTie() == 9) {
+            alert("TIed");
+            gameBoard.clearGameBoardArray();
         }
         console.log(gameBoard.getCurrentPlayer().getName());
     }
@@ -113,7 +218,7 @@ const displayController = (() => {
         
     };
     
-    return{boardCreator}
+    return{boardCreator, refreshBoard}
 
 })();
 
